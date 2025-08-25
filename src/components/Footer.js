fea8ad852934aec1d01de96c292804ad10766823
Veraeze <Veraeze18@gmail.com>
2025-08-25 16:48:12 +0100

    Added Footer logic

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-gray-400 text-center py-4">
+      © {new Date().getFullYear()} Omah Tech Ltd. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
