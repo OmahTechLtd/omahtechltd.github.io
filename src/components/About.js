a7bb51d00b61bfe48ecb0718a51ae546e8d1d6b6
Veraeze <Veraeze18@gmail.com>
2025-10-14 16:56:26 +0100

    Linked CustomModels 'Book a Consultation' button to ConsultationModal form

--- src/components/About.js
+++ src/components/About.js
@@ -9,14 +9,7 @@ export default function About() {
           Our mission is to drive digital transformation by leveraging local data and intelligent insights.
         </p>
         <p className="mt-8 text-center">
-  <a
-    href="/docs/OmahTech_Company_Policy.pdf"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="text-blue-400 hover:underline"
-  >
-    View our Company Policy
-  </a>
+  
 </p>
       </div>
     </section>
