e0c2b1e832926a1f620240fe2aaa49977cb6feb6
Veraeze <Veraeze18@gmail.com>
2025-12-06 13:46:48 +0100

    test auto deploy on render

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
 import Subscriber from "./models/Subscriber.js";
 import trainingSetupRoutes from "./routes/trainingSetup.js";
